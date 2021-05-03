26eb3411a96a0c662d78cd19fab1cc2e10f1f778
Kim_myung_ok <kmo9705@naver.com>
2021-05-04 01:48:38 +0900

    자동으로 전화 연결되는 부분에서 프로그램내 충돌 해결

--- app/src/main/java/com/example/call_test_page/MainActivity.java
+++ app/src/main/java/com/example/call_test_page/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.call_test_page;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,23 +19,33 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
 
-public class MainActivity<CALL_PHONE> extends AppCompatActivity {
+import static android.Manifest.*;
+import static android.Manifest.permission.*;
+import static android.content.pm.PackageManager.*;
+
+
+public class MainActivity extends AppCompatActivity {
 
     private Button buttonSend;
     private EditText textPhoneNo;
     private EditText textSMS;
-    private static final int MY_PERMISSION_STORAGE = 1111;
+    private static int REQUEST_CALL_PHONE = 1111;
     String[] permission_list = {
-            Manifest.permission.CALL_PHONE
-//            Manifest.permission.SEND_SMS
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_PHONE_NUMBERS
+
     };
     //충격센서의 충격 여부
     boolean bluetooth = true;
@@ -42,8 +56,6 @@ public class MainActivity<CALL_PHONE> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -52,11 +64,10 @@ public class MainActivity<CALL_PHONE> extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
         checkPermission();
 
-        ContextCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE);
         //충격센서의 충격에 따라 call_sos() 실행
-        if(bluetooth) call_sos();
-    }
+        if(bluetooth==true) call_sos();
 
+    }
 
     public void checkPermission() {
         //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
@@ -80,17 +91,23 @@ public class MainActivity<CALL_PHONE> extends AppCompatActivity {
                 if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
 
                 } else {
-                    Toast.makeText(getApplicationContext(), "앱 권한 설정하세요", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "앱권한설정하세요", Toast.LENGTH_LONG).show();
                 }
             }
         }
     }
 
+    
+    /* ##############
+       자동 통화 걸기
+       ##############*/
     public void call_sos() {
+//    public void call_Button(View v) {
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-        Toast.makeText(getApplicationContext(), "call_sos();", Toast.LENGTH_LONG).show();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                Toast toast = Toast.makeText(MainActivity.this, "권한 없음", Toast.LENGTH_SHORT);
+                toast.show();
                 // 권한 없음
             } else {
                 Intent call = new Intent(Intent.ACTION_CALL, Uri.parse("tel:01072904078"));
@@ -100,27 +117,6 @@ public class MainActivity<CALL_PHONE> extends AppCompatActivity {
         }
         // OS가 Marshmallow 이전일 경우 권한체크를 하지 않는다.
         else {
-
-        }
-    }
-
-    //전화걸기 버튼 누름시, 통화걸기
-    public void call_Button(View v) {
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-        Toast.makeText(getApplicationContext(), "call_Button();", Toast.LENGTH_LONG).show();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-                checkPermission();
-                // 권한 없음
-            } else {
-                Intent call = new Intent(Intent.ACTION_CALL, Uri.parse("tel:01072904078"));
-                startActivity(call);
-                // 권한 있음
-            }
-        }
-        // OS가 Marshmallow 이전일 경우 권한체크를 하지 않는다.
-        else {
-
         }
     }
 }
